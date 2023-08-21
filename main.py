8e4766addc2cd9dd0eeaff93a22ecad26d4d29c9
nikhilmirandaunbxd <nikhil.miranda@gmail.com>
2023-08-21 21:46:11 +0530

    feat: added health check api

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/health")
+def root():
+    return {"message": "I am Alive !!"}

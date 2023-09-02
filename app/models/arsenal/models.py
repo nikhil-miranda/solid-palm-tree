09012845f2b4aaca354c0780d22f3f40e3a3dafb
nikhil-miranda <nikhil.miranda@gmail.com>
2023-09-02 13:59:15 +0530

    feat: added new api to get all messages list from mongodb

--- app/models/arsenal/models.py
+++ app/models/arsenal/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -5,3 +8,4 @@ class Message(BaseModel):
     name: str
     email: str
     message: str
+    created_at: Optional[str] = None
--- app/services/arsenal/services.py
+++ app/services/arsenal/services.py
@@ -4,7 +4,17 @@ from app.databases.mongodb import dao
 from app.models.arsenal import models
 
 
+def fetch_messages(page_number: int = 1, page_size: int = 10):
+    try:
+        return dao.read_all_messages(page_number=page_number, page_size=page_size)
+    except Exception as e:
+        print_exc()
+        return {"error": "Failed to fetch messages.", "e": str(e)}
+
+
 def store_message(payload: models.Message):
+    if payload.name is None and payload.email is None and payload.message is None:
+        return {"message": "Details were sent empty."}
     try:
         return dao.create_message(payload=payload)
     except Exception as e:
